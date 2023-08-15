b1d894535fae74a4c47d36307528fc6b4125523f
PosledniAmir <posledniamir@tuta.io>
2023-08-15 20:03:02 +0200

    Update trie-tests.lisp

--- tests/trie-tests.lisp
+++ tests/trie-tests.lisp
@@ -4,3 +4,17 @@
 (in-package :trie-tests)
 
 (def-suite* trie-tests :in funcl-suite)
+
+(test trie-test
+  (let ((result (trie (list "atest" "atest")
+                      (list "btest" "btest")
+                      (list "ctest" "ctest")
+                      (list "testa" "testa")
+                      (list "testb" "testb")
+                      (list "testc" "testc"))))
+    (is (string= "atest" (look-for "atest" result))
+        (string= "btest" (look-for "btest" result))
+        (string= "ctest" (look-for "ctest" result))
+        (string= "testa" (look-for "testa" result))
+        (string= "testb" (look-for "testb" result))
+        (string= "testc" (look-for "testc" result)))))
