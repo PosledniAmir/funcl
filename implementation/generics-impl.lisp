74f56fbe34bb67ec6c9161ab30d786fe346a3c4f
PosledniAmir <posledniamir@tuta.io>
2023-09-25 19:07:17 +0200

    Update generics-impl.lisp

--- implementation/generics-impl.lisp
+++ implementation/generics-impl.lisp
@@ -9,6 +9,53 @@
 (defmethod equal? ((first character) (second character))
   (char= first second))
 
+(defmethod equal? ((first symbol) (second character))
+  nil)
+
+(defmethod equal? ((first character) (second symbol))
+  nil)
+
+(defmethod compare ((first symbol) (second symbol))
+  (let ((a (string first))
+        (b (string second)))
+    (cond
+      ((string= a b) 0)
+      ((string< a b) -1)
+      ((string> a b) 1))))
+
+(defmethod compare ((first character) (second character))
+  (cond
+    ((char= first second) 0)
+    ((char< first second) -1)
+    ((char> first second) 1)))
+
+(defmethod compare ((first null) (second character))
+  -1)
+
+(defmethod compare ((first character) (second null))
+  1)
+
+(defmethod compare ((first cons) (second cons))
+  (cond
+    ((null first) -1)
+    ((null second) 1)
+    (t (let ((result (compare (car first) (car second))))
+         (cond
+           ((= 0 result) (compare (cdr first) (cdr second)))
+           (t result))))))
+
+(defmethod compare ((first cons) (second null))
+  1)
+
+(defmethod compare ((first null) (second cons))
+  -1)
+
+(defmethod compare ((first integer) (second integer))
+  (cond
+    ((= first second) 0)
+    ((< first second) -1)
+    ((> first second) 1)))
+
 (defmethod to-list ((collection string))
   (coerce collection 'list))
 
