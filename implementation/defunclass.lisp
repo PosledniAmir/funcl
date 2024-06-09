922ca050686b168a328a970ee4d4dde29dd9115e
PosledniAmir <posledniamir@tuta.io>
2024-06-09 22:31:37 +0200

    we started with defunclass refactor

--- implementation/defunclass.lisp
+++ implementation/defunclass.lisp
@@ -8,37 +8,32 @@
   "produces code for reader slots in defunclass"
   (let* ((name (first slot))
          (initform (second slot))
+         (initarg (intern (string name) "KEYWORD"))
+         (reader (intern (format nil "@~a" name))))
+    `(,name :initarg ,initarg :initform ,initform :reader ,reader)))
+
+(defun make-slot-name-pair (slot)
+  "produces (:name name)"
+  (let* ((name (first slot))
          (initarg (intern (string name) "KEYWORD")))
-    `(,name :initarg ,initarg :initform ,initform :reader ,name)))
+    (list initarg name)))
 
-(defun make-slot-text (slot)
-  "produces format string printing a reader in defunclass"
-  (concatenate 'string (format nil ":~a" slot) " ~a"))
-
-(defun intertwine (what where)
-  "basically zip, but the second list is generated from single item"
+(defun make-slot-name-pairs (slots)
+  "produces flat list of slot-name-pairs"
   (cond
-    ((null where) nil)
-    ((null (cdr where)) (cons (car where) nil))
-    (t (cons (car where) (cons what (intertwine what (cdr where)))))))
-
-(defun make-print-text (slots)
-  "produces string to printing defunclass by format"
-  (let ((texts (map 'list #'make-slot-text slots)))
-    (apply #'concatenate 'string (intertwine " " texts))))
-
-(defun make-slot-call (slot)
-  "produces code calling slot of an obj - used in defunclass macro for printing"
-  `(,slot obj))
+    ((null slots))
+    (t (let* ((result (make-slot-name-pair (first slots)))
+              (init (first result))
+              (var (second result)))
+         (cons init (cons var (make-slot-name-pairs (rest slots))))))))
 
 (defmacro defunclass (name parents slots &body body)
   "defines functional class, that means that all slots are readers"
   (let* ((transformed (map 'list #'transform-slot slots))
-         (slot-names (map 'list #'car slots))
-         (text (make-print-text slot-names))
-         (items (map 'list #'make-slot-call slot-names)))
+         (constructor (intern (format nil "<~a>" name)))
+         (slot-name-pairs (make-slot-name-pairs slots)))
     `(progn
        (defclass ,name ,parents ,transformed ,@body)
-       (defmethod print-object ((obj ,name) stream)
-         (print-unreadable-object (obj stream :type t)
-           (format stream ,text ,@items))))))
+       (defun ,constructor (&key ,@slots)
+         ,(format nil "Constructor for class ~a" name)
+         (make-instance (quote ,name) ,@slot-name-pairs)))))
--- tests/defunclass-tests.lisp
+++ tests/defunclass-tests.lisp
@@ -8,9 +8,6 @@
 (defunclass testing-class () ((item0 nil) (item1 1)) (:documentation "testing class"))
 
 (test defunclass-test0
-  (let ((result (make-instance 'testing-class :item0 25 :item1 nil)))
-    (is (string= "#<TESTING-CLASS :ITEM0 25 :ITEM1 NIL>"
-                 (format nil "~a" result))
-        (= 25 (item0 result))
-        (null (item1 result)))))
-
+  (let ((result (<testing-class> :item0 25 :item1 nil)))
+    (is (= 25 (@item0 result))
+        (null (@item1 result)))))
