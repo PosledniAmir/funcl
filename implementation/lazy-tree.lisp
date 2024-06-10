7edc794eda2332f5f76b9b544cec34fe91a68a16
PosledniAmir <posledniamir@tuta.io>
2024-06-10 20:36:45 +0200

    Update lazy-tree.lisp

--- implementation/lazy-tree.lisp
+++ implementation/lazy-tree.lisp
@@ -10,8 +10,8 @@
   (defunclass lazy-tree ()
       ((count 0)
        (value (error "slot value must be set in lazy-tree"))
-       (left (make-instance 'lazy-tree-empty))
-       (right (make-instance 'lazy-tree-empty)))
+       (left (<lazy-tree-empty>))
+       (right (<lazy-tree-empty>)))
     (:documentation "Lazy B\[alpha\] tree")))
 
 (defmethod nil? ((obj lazy-tree-empty))
@@ -20,3 +20,47 @@
 (defmethod nil? ((obj lazy-tree))
   nil)
 
+(defmethod to-list ((obj lazy-tree-empty))
+  '())
+
+(defmethod to-list ((obj lazy-tree))
+  (concatenate 'list
+               (to-list (@left obj))
+               (cons (@value obj)
+                     (to-list (@right obj)))))
+
+(defun split-at (index list acc)
+  (cond
+    ((<= index 0) (values (reverse acc) list))
+    (t (split-at (- index 1)
+                 (rest list)
+                 (cons (first list) acc)))))
+
+(defun from-sorted (size list)
+  (cond
+    ((<= size 0) (values (<lazy-tree-empty>) list))
+    (t (let* ((result (multiple-value-list (truncate size 2)))
+              (left-size (- (+ (first result) (second result)) 1))
+              (right-size (first result))
+              (left-result (from-sorted left-size list))
+              (left-tree (first left-result))
+              (value (second (first left-result)))
+              (right-result (from-sorted right-size (second (rest left-result))))
+              (right-tree (first right-result))
+              (result-list (second right-result)))
+         (values (<lazy-tree> :count size
+                              :value value
+                              :left left-tree
+                              :right right-tree)
+                 result-list)))))
+
+(defmethod head ((obj lazy-tree))
+  (let ((v (@value obj))
+        (l (@left obj)))
+    (cond
+      ((nil? l) v)
+      (t (head l)))))
+
+(defmethod concat (element (obj lazy-tree-empty))
+  (<lazy-tree> :count 1
+               :value element))
