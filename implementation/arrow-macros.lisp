f4da6a0f135cc22fb7e50216405168307267be68
PosledniAmir <posledniamir@tuta.io>
2024-06-23 18:46:50 +0200

    added a test to ->

--- implementation/arrow-macros.lisp
+++ implementation/arrow-macros.lisp
@@ -4,11 +4,16 @@
 
 (in-package :arrow-macros)
 
+(defun funcall? (list)
+  (and (listp list)
+       (eq 'funcall (first list))))
+
 (defmacro -> (expression &body body)
   "basic arrow macro (-> 5 (+ 3 2) (* 2)) creates (* (+ 5 3 2) 2)"
   (let ((transformed (reduce
                       (lambda (acc form)
                         (cond
+                          ((funcall? form) `(,(first form) ,(second form) ,acc ,@(rest (rest form))))
                           ((listp form) `(,(first form) ,acc ,@(rest form)))
                           (t `(,form ,acc))))
                       body
--- tests/arrow-macros-tests.lisp
+++ tests/arrow-macros-tests.lisp
@@ -8,3 +8,7 @@
 (test arrow-test0
       (let ((result (arrow-macros:-> 5 (+ 1) (/ 2) (* 3))))
         (is (= 9 result))))
+
+(test arrow-test1
+      (let ((result (arrow-macros:-> 5 (funcall (lambda (x) (+ x 1))) (/ 2) (* 3))))
+        (is (= 9 result))))
